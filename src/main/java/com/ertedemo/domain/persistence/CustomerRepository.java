858faf63d94a09c2aaa233228052471ccb1931d6
Kaitozerob <u202223781@upc.edu.pe>
2025-05-17 16:05:48 -0500

    feat: add JPA repository for Customer

--- src/main/java/com/ertedemo/domain/persistence/CustomerRepository.java
+++ src/main/java/com/ertedemo/domain/persistence/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.ertedemo.domain.persistence;
 
-public interface CustomerRepository {
+import com.ertedemo.domain.model.entites.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
