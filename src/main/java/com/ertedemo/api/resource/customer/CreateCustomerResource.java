4a0e9e9d08de9786d0e889bc3eca2cf359a21018
Kaitozerob <u202223781@upc.edu.pe>
2025-05-17 16:11:24 -0500

    feat: add resource for creating customers

--- src/main/java/com/ertedemo/api/resource/customer/CreateCustomerResource.java
+++ src/main/java/com/ertedemo/api/resource/customer/CreateCustomerResource.java
@@ -1,4 +1,10 @@
 package com.ertedemo.api.resource.customer;
 
+import lombok.Data;
+
+@Data
 public class CreateCustomerResource {
+    private Long landlordId;
+    private Long tenantId;
+    private String status;
 }
