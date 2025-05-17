de8cb984fdf4ed62062e1cefe53a65d4742f452d
Kaitozerob <u202223781@upc.edu.pe>
2025-05-17 16:00:51 -0500

    feat: add Customer entity

--- src/main/java/com/ertedemo/domain/model/entites/Customer.java
+++ src/main/java/com/ertedemo/domain/model/entites/Customer.java
@@ -1,4 +1,39 @@
 package com.ertedemo.domain.model.entites;
 
+import com.ertedemo.api.resource.customer.CreateCustomerResource;
+import com.ertedemo.domain.model.valueobjects.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "landlord_id", nullable = false)
+    private Landlord landlord;
+
+    @ManyToOne
+    @JoinColumn(name = "tenant_id", nullable = false)
+    private Tenant tenant;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status; // e.g., "accepted", "pending", "rejected"
+
+    public Customer() {
+    }
+
+    public Customer(CreateCustomerResource resource, Landlord landlord, Tenant tenant) {
+        this.landlord = landlord;
+        this.tenant = tenant;
+        this.status = Status.valueOf(resource.getStatus());
+    }
 }
