7981f238dccd68b4657e41d80c39c50b7c5953aa
Kaitozerob <u202223781@upc.edu.pe>
2025-05-17 16:20:11 -0500

    feat: add resource DTO for customer data

--- src/main/java/com/ertedemo/api/resource/customer/CustomerResponse.java
+++ src/main/java/com/ertedemo/api/resource/customer/CustomerResponse.java
@@ -1,4 +1,20 @@
 package com.ertedemo.api.resource.customer;
 
+import com.ertedemo.domain.model.entites.Customer;
+import com.ertedemo.domain.model.valueobjects.Status;
+import lombok.Data;
+
+@Data
 public class CustomerResponse {
+    private Long id;
+    private Long landlordId;
+    private Long tenantId;
+    private Status status;
+
+    public CustomerResponse(Customer customer) {
+        this.id = customer.getId();
+        this.landlordId = customer.getLandlord().getId();
+        this.tenantId = customer.getTenant().getId();
+        this.status = customer.getStatus();
+    }
 }
