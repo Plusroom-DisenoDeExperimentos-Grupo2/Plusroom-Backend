1be2a0548d217a4a0ee247d683ca90385a9e2d18
Andrea O'Higgins <u20221b178@upc.edu.pe>
2025-05-17 11:47:14 -0500

    feat(config): add CORS policy, OpenAPI docs, and exception handling

---
+++ src/main/java/com/ertedemo/shared/documentation/OpenApiConfiguration.java
@@ -0,0 +1,27 @@
+package com.ertedemo.shared.documentation;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi(){
+        return new OpenAPI()
+                .info(new Info().title("PlusRoom API")
+                        .description(
+                                "PlusRoom Platform application REST API documentation")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("https://springgoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("PlusRoom platform with Documentation")
+                        .url("https://acme-learning-platform.wik.github.org/docs"));
+    }
+}
---
+++ src/main/java/com/ertedemo/shared/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package com.ertedemo.shared.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
