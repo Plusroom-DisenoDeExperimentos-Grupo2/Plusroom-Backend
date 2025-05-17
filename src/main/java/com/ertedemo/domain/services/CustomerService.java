da084f0c6264057751a1e960f7112f2566a1ef7f
Kaitozerob <u202223781@upc.edu.pe>
2025-05-17 16:08:18 -0500

    feat: define service interface for Customer logic

--- src/main/java/com/ertedemo/domain/services/CustomerService.java
+++ src/main/java/com/ertedemo/domain/services/CustomerService.java
@@ -1,4 +1,17 @@
 package com.ertedemo.domain.services;
 
+import com.ertedemo.domain.model.entites.Customer;
+import com.ertedemo.domain.model.valueobjects.Status;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerService {
+    List<Customer> getAll();
+    Optional<Customer> getById(Long id);
+    Customer create(Customer customer);
+    Optional<Customer> update(Customer customer);
+    void delete(Long id);
+
+    void updateStatus(Customer customer, Status status);
 }
